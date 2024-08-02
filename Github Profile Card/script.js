f9f8386769350df6476017abe0a02b3349126968
Rahul Baraiya <168319508+geeksrahul@users.noreply.github.com>
2024-08-02 17:20:43 +0530

    Update script.js

--- Github Profile Card/script.js
+++ Github Profile Card/script.js
@@ -8,24 +8,28 @@ const user = {
     github : document.getElementById('githubLink')
 }
 
-const username = "geeksrahul";
-const URL = `https://api.github.com/users/${username}`;
-const xhr = new XMLHttpRequest();
-xhr.open('GET', URL, true);
-xhr.onreadystatechange = ()=> {
-    if(xhr.readyState === 4) {
-        let data = JSON.parse(xhr.responseText);
-        console.log(data);
-        user.name.textContent = data.name;
-        user.username.textContent = `@${data.login}`;
-        user.pfp.src = data.avatar_url;
-        user.bio.textContent = data.bio;
-        user.followers.textContent = data.followers;
-        user.following.textContent = data.following;
-        user.github.href = data.html_url;
+function getData(username) {
+    const URL = `https://api.github.com/users/${username}`;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', URL, true);
+    xhr.onreadystatechange = ()=> {
+        if(xhr.readyState === 4) {
+            let data = JSON.parse(xhr.responseText);
+            console.log(data);
+            user.name.textContent = data.name;
+            user.username.textContent = `@${data.login}`;
+            user.pfp.src = data.avatar_url;
+            user.bio.textContent = data.bio;
+            user.followers.textContent = data.followers;
+            user.following.textContent = data.following;
+            user.github.href = data.html_url;
+        }
     }
+    xhr.send();
 }
-xhr.send();
 
+document.getElementById("btnSearch").addEventListener("click", ()=>{
+    getData(document.getElementById("searchInput").value);
+});
 
 
